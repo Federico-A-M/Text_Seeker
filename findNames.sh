97133b9484d8eaad34c604bc2e879b7d13041df3
Federico.a.m <fede.acrata@gmail.com>
2022-07-22 10:39:52 -0300

    Ejercicio 3 terminado

---
+++ findNames.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+###############################################################################
+#	findNames.sh
+#	Identificación de nombres propios (se identifican sólo si están en este formato
+#	Nnnnnnnnn), aunque la palabra no sea un nombre propio realmente.
+#	
+#	Ejemplos: Mateo, Estonoesunnombre, Ana.
+###############################################################################
+
+
+
+REGEX="[A-Z]\w+$" #expresion regular que matchea solo con palabras y evita los numeros
+
+           #chapter37.txt
+FILE=$( cat chapter37.txt | tr -d "[:punct:]" | tr -d "..." | tr " " "\n")
+
+PROPERNOUNS=()
+i=0
+
+for word in $FILE;
+do 
+     if [[ $word =~ $REGEX ]]; then
+	     PROPERNOUNS[$i]=$word
+	     i=$(( $i + 1 ))
+     fi  
+         
+done
+
+
+echo "Total of proper nouns: "
+echo ${#PROPERNOUNS[@]}
+echo ""
+echo "Listing :"
+
+for j in ${PROPERNOUNS[@]}
+do
+	echo -n "$j /  "
+done	
+
+
+
+
+exit 0
